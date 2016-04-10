d1031ee4b3e81c456f6176615807f7f43edc5b68
Anna Sobolewska <sobolewska.anna@gmail.com>
2016-04-10 15:53:42 -0700

    added functionality to the I'm not feeling it button. It now shows another search result

--- public/scripts/search_result.js
+++ public/scripts/search_result.js
@@ -6,21 +6,33 @@ $(document).ready(function(){
   var windowPathSplit = windowPath.split("/");
   var id = windowPathSplit[2];
   var url = '/api/searches/' + id + '/results';
+  var outsideResults;
 
   $.ajax({
     method: 'GET',
     url: url,
-    success: handleSucces,
+    success: handleSuccess,
     error: handleError
   });
 
+  $('#notFeelingItButton').on('click', function (event) {
+    renderResult(outsideResults);
+  });
+
+  function handleSuccess (results) {
+    outsideResults = results;
+    renderResult(outsideResults);
+  }
+
 });
 
-function handleSucces (json) {
-  console.log('hello');
-  console.log(json);
-  $('.results').append(json);
-}
+  function renderResult (results) {
+    var resultsAmount = results.result.length;
+    var randomNumber = Math.floor((Math.random() * resultsAmount) + 0);
+    var venueName = results.result[randomNumber].venueName;
+    var venueLocation = results.result[randomNumber].venueLocation;
+    $('.results').html(venueName);
+  }
 
 function handleError (err) {
   console.log ('there is an error', err);
