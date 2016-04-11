fe08f0cee88e5c4897b510a4e31e40409d00ca24
Anna Sobolewska <sobolewska.anna@gmail.com>
2016-04-10 20:28:11 -0700

    added handlebars to recent search phrases

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,15 +2,22 @@ console.log('sanity check');
 
 $(document).ready(function(){
 
+  // compile handlebars template
+  var source = $('#search-template').html();
+  var searchTemplate = Handlebars.compile(source);
+
   populateWithRecentSearches();
   function populateWithRecentSearches () {
     $.ajax({
       method: 'GET',
       url: '/api/searches',
       success: function findSearches (searches) {
-        searches.forEach(function (search) {
-          $('.recent-searches').append(search.query);
-        });
+        var searchesHtml = searchTemplate({searches: searches});
+        $('#recentSearches').append(searchesHtml);
+
+        // searches.forEach(function (search) {
+        //   $('.recent-searches').append(search.query);
+        // });
         // setTimeout(populateWithRecentSearches(), 5000);
       },
       error: handleError
